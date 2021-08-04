59e9854cad786ae8c6b84e55eeb0d5b7fa961679
chuuuya12 <87469127+chuuuya12@users.noreply.github.com>
2021-08-05 06:33:13 +0800

    Chat

--- server.js
+++ server.js
@@ -59,6 +59,6 @@ io.on('connection', socket => {
 });
 });
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
