003e4e5b3915e435e69e83137a85db9450f5785b
chuuuya12 <87469127+chuuuya12@users.noreply.github.com>
2021-08-05 12:22:18 +0800

    Chat

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages');
 const roomName = document.getElementById('room-name');
 const userList = document.getElementById('users');
+var qs = require('qs');
 
 
 // Get username and room from URL
