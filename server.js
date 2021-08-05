ca8d66e797499fd264cb986618932f538279c79f
chuuuya12 <87469127+chuuuya12@users.noreply.github.com>
2021-08-05 08:41:01 +0800

    Chat

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-app.use(express.static(path.join(__dirname, '../Full')));
+app.use(express.static(path.join(__dirname, './utils/users')));
 
 const botName = 'Chat Bot';
 
