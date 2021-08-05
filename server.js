39be3212564a90f441f631d6de534cebfa595cbd
chuuuya12 <87469127+chuuuya12@users.noreply.github.com>
2021-08-05 19:38:34 +0800

    Chat

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const {
 } = require('./utils/users');
 
 const app = express();
-const server = https.createServer(app);
+const server = http.createServer(app);
 const io = socketio(server);
 
 // Set static folder
