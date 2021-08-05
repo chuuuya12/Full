6ba369aa87d504e5099fa9d7c1b11af87c92c070
chuuuya12 <87469127+chuuuya12@users.noreply.github.com>
2021-08-05 12:53:20 +0800

    Chat

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { username, room } = Qs.parse(location.search, {
   ignoreQueryPrefix: true,
 });
 
-const socket = io('/');
+const socket = io('https://chatnonymous-bot-deploy.herokuapp.com/');
 
 // Join chatroom
 socket.emit('joinRoom', { username, room });
