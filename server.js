4f0a614eb1d19e1dd0c298bbf89e6c2a476ee2a5
chuuuya12 <87469127+chuuuya12@users.noreply.github.com>
2021-08-05 18:29:43 +0800

    Chat

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const formatMessage = require('./utils/messages');
@@ -10,8 +9,7 @@ const {
   getRoomUsers
 } = require('./utils/users');
 
-const socketUrl = '/';
-
+const socketUrl = '/'
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
