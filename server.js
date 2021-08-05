f18eed5b06dc276344be1dbb95de93cc4239a331
chuuuya12 <87469127+chuuuya12@users.noreply.github.com>
2021-08-05 10:00:32 +0800

    Chat

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const socketio = require('socket.io');
 const formatMessage = require('./utils/messages');
 const { userJoin, getCurrentUser,userLeave, getRoomUsers } = require('./utils/users');
 
-const app = express();
+const app = express('https://chatnonymous-bot-deploy.herokuapp.com/');
 const server = http.createServer(app);
 const io = socketio(server);
 
