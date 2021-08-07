3f5d4b4ed8812ec898d57d5f7ea873efe92e4290
chuuuya12 <87469127+chuuuya12@users.noreply.github.com>
2021-08-07 08:58:43 +0800

    Chat

--- index.js
+++ index.js
@@ -128,15 +128,15 @@ function startup() {
   colorWell.select();
 }
 function updateFirst(event) {
-  var meta = document.querySelector("p");
+  var meta = document.querySelector("user.username");
 
-  if (p) {
-    p.style.color = event.target.value;
+  if (user.username) {
+    user.username.style.color = event.target.value;
   }
 }
 
 function updateAll(event) {
-  document.querySelectorAll("p").forEach(function(p) {
-    p.style.color = event.target.value;
+  document.querySelectorAll("user.username").forEach(function(p) {
+    user.username.style.color = event.target.value;
   });
 }
