25bcefe12cb8b4c5ce36d585717d15d9ba72deca
chuuuya12 <87469127+chuuuya12@users.noreply.github.com>
2021-08-07 12:16:47 +0800

    Chat

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const roomName = document.getElementById('room-name');
 const userList = document.getElementById('users');
 
 // Get username and room from URL
-const { username, room,ucolor } = Qs.parse(location.search, {
+const { username, room } = Qs.parse(location.search, {
   ignoreQueryPrefix: true,
 });
 
@@ -12,7 +12,7 @@ const socket = io('https://chatnonymous-bot-deploy.herokuapp.com/');
 
 
 // Join chatroom
-socket.emit('joinRoom', { username, room, ucolor });
+socket.emit('joinRoom', { username, room });
 
 // Get room and users
 socket.on('roomUsers', ({ room, users }) => {
@@ -57,7 +57,6 @@ function outputMessage(message) {
   const p = document.createElement('p');
   p.classList.add('meta');
   p.innerText = message.username;
-  p.style=`color: ${message.ucolor}`
   p.innerHTML += `<span> ${message.time}</span>`;
   div.appendChild(p);
   const para = document.createElement('p');
@@ -96,3 +95,29 @@ document.getElementById('leave-btn').addEventListener('click', () => {
 
 
 
+// Color pcker
+var colorWell;
+var defaultColor = "#0000ff";
+
+window.addEventListener("load", startup, false);
+
+function startup() {
+  colorWell = document.querySelector("#colorWell");
+  colorWell.value = defaultColor;
+  colorWell.addEventListener("input", updateFirst, false);
+  colorWell.addEventListener("change", updateAll, false);
+  colorWell.select();
+}
+function updateFirst(event) {
+  var meta = document.querySelector("p");
+
+  if (p) {
+    p.style.color = event.target.value;
+  }
+}
+
+function updateAll(event) {
+  document.querySelectorAll("p").forEach(function(p) {
+    p.style.color = event.target.value;
+  });
+}
