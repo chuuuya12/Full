b760f9be9bfd999a58cb35511bd5522bcf3d79ba
chuuuya12 <87469127+chuuuya12@users.noreply.github.com>
2021-08-10 08:43:40 +0800

    Chat

--- utils/messages.js
+++ utils/messages.js
@@ -1,11 +1,6 @@
 const moment = require('moment');
 
-function moment() {
-    const currentDate = new Date()
-    const secondsRatio = currentDate.getSeconds() / 60
-    const minutesRatio = ( secondsRatio + currentDate.getMinutes()) / 60
-    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
-}
+
 
 function formatMessage(username,ucolor, text) {
     return {
