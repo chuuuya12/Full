07836784ed6cb5ee37733daa0353fbd8f8a930f2
chuuuya12 <87469127+chuuuya12@users.noreply.github.com>
2021-08-14 15:25:11 +0800

    Chat

--- utils/messages.js
+++ utils/messages.js
@@ -5,6 +5,7 @@ const moment = require('moment');
 function formatMessage(username,ucolor, text) {
     return {
         username,
+        character,
         ucolor,
         text,
         time: moment().format('h:mm:a')
