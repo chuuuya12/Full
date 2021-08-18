962f5c93a0d38cbcea68d58f69690e480ebe2124
chuuuya12 <87469127+chuuuya12@users.noreply.github.com>
2021-08-18 20:16:30 +0800

    Chat

--- utils/messages.js
+++ utils/messages.js
@@ -7,7 +7,7 @@ function formatMessage(username,ucolor, text) {
         username,
         ucolor,
         text,
-        time: moment().valueOf()
+        time: moment().format('h:mm:a')
     }
 }
 
