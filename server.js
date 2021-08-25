6d2061728c3e70a9de3ccf6ca3fde91937769f73
chuuuya12 <87469127+chuuuya12@users.noreply.github.com>
2021-08-25 22:22:36 +0800

    Chat

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const formatMessage = require('./utils/messages');
+self.server.manager.rooms.keys()
 const {
   userJoin,
   getCurrentUser,
