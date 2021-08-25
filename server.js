0ced0e7730e93e0f13e0ba84614270bbc017a608
chuuuya12 <87469127+chuuuya12@users.noreply.github.com>
2021-08-26 00:52:47 +0800

    Chat

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
-const { createClient } = require('redis');
-const redisAdapter = require('@socket.io/redis-adapter');
 const formatMessage = require('./utils/messages');
 const {
   userJoin,
