03cbcd3bb482965d980fd90e60ce1a90297f5499
chuuuya12 <87469127+chuuuya12@users.noreply.github.com>
2021-08-30 03:16:59 +0800

    Chat

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ function outputMessage(message) {
   para.innerText = message.text;
   div.appendChild(para);
   document.querySelector('.chat-messages').appendChild(div);
-  time: moment(message.formatMessage).time('h:mm a')
+  time: moment(message.time).format('h:mm a')
 }
 
 // Add room name to DOM
