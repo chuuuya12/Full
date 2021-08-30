c4a7b98df530d9431ed8bdf1308cc9c763df6c42
chuuuya12 <87469127+chuuuya12@users.noreply.github.com>
2021-08-30 22:03:20 +0800

    Chat

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
-const roomlist = document.getElementById('room_list');
+const room = [];
+const roomList = document.getElementById('room_list');
 
+function roomList(room) {
+    roomList.innerHTML = '';
+    room.forEach((room) => {
+      const li = document.createElement('li');
+      li.innerText = room.roomList;
+      roomList.appendChild(li);
+    });
+  }
