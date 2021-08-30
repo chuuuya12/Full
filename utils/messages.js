3765b94ea17f3e10427e0c7e476cce0452dd73ba
chuuuya12 <87469127+chuuuya12@users.noreply.github.com>
2021-08-30 17:46:47 +0800

    Chat

--- utils/messages.js
+++ utils/messages.js
@@ -1,7 +1,8 @@
-const generateMessage= (username,msg)=>{
+const generateMessage= (username,ucolor, text)=>{
 
     return {
     username,
+    ucolor,
     text: msg,
     createdAt: new Date().getTime()
     }
