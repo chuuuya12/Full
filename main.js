052cf9c4e7a74c6669f17b3208f73fe591a52271
chuuuya12 <87469127+chuuuya12@users.noreply.github.com>
2021-08-30 19:27:13 +0800

    Chat

--- main.js
+++ main.js
@@ -1,22 +1,2 @@
 const roomlist = document.getElementById('room_list');
 
-const {room} = Qs.parse(location.search, {
-    ignoreQueryPrefix: true,
-  });
-
-  const socket = io('https://chatnonymous-bot-deploy.herokuapp.com/chat.html');
-
-
-  socket.on('roomList', ({ room }) => {
-    outputRoomName(room);
-  });
-
-  function outputRoomList(users) {
-    roomList.innerHTML = '';
-    room.forEach((user) => {
-      const li = document.createElement('li');
-      li.innerText = user.username;
-      userList.appendChild(li);
-    });
-  }
-  
