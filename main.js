d6544f632d48324511dd201640fc6ceb15a2cd2f
chuuuya12 <87469127+chuuuya12@users.noreply.github.com>
2021-08-31 01:39:26 +0800

    Chat

--- main.js
+++ main.js
@@ -1,19 +1,9 @@
-const socket = io()
 const list = document.getElementById('room_list');
 socket.emit('joinRoom', { list });
 socket.on('roomUsers', ({ list }) => {
     outputList(list)
   });
 
-function outputList(room) {
+  function outputRoomName(room) {
     list.innerText = room;
-  }
-
-  function outputList(list) {
-    outputList.innerHTML = '';
-    room.forEach((list) => {
-      const li = document.createElement('li');
-      li.innerText = room.list;
-      List.appendChild(li);
-    });
   }
